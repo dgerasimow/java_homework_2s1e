8a333829fcdbfc0ee1921c90eb9aeab44e8f73ae
Danil Gerasimov <lil.wayne.rap.go@gmail.com>
2021-09-10 17:51:01 +0300

    initial and last commit

---
+++ src/main/java/ru/kpfu/itis/gerasimov/net/HttpClientImplementation.java
@@ -0,0 +1,105 @@
+package ru.kpfu.itis.gerasimov.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class HttpClientImplementation implements HttpClient {
+
+    @Override
+    public String get(String url, Map<String, String> headers, Map<String, String> params) {
+        try {
+
+            URL getURL = new URL(makeURLWithParams(params, url));
+            HttpURLConnection getConnection = (HttpURLConnection) getURL.openConnection();
+            for (String key :
+                    headers.keySet()) {
+                getConnection.setRequestProperty(key, headers.get(key));
+            }
+
+            getConnection.setRequestMethod("GET");
+
+            getConnection.setConnectTimeout(5000);
+            getConnection.setReadTimeout(5000);
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(getConnection.getInputStream()))) {
+                StringBuilder content = new StringBuilder();
+                String input;
+                while ((input = reader.readLine()) != null) {
+                    content.append(input);
+                }
+                getConnection.disconnect();
+                return content.toString();
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "null";
+    }
+
+    @Override
+    public String post(String url, Map<String, String> headers, Map<String, String> params) {
+        try {
+            URL postURL = new URL(url);
+            HttpURLConnection postConnection = (HttpURLConnection) postURL.openConnection();
+            postConnection.setRequestMethod("POST");
+
+            for (String key :
+                    headers.keySet()) {
+                postConnection.setRequestProperty(key, headers.get(key));
+            }
+
+            int kostilCounter = 1;
+            String jsonInputString = "{";
+            for (String key :
+                    params.keySet()) {
+                jsonInputString += "\""+ key + "\"" +":" + "\"" + params.get(key) + "\"";
+                if (kostilCounter < params.keySet().size()) {
+                    jsonInputString += ",";
+                } else {
+                    jsonInputString += "}";
+                }
+                kostilCounter++;
+            }
+
+            postConnection.setDoOutput(true);
+            try (OutputStream outputStream = postConnection.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(input);
+            }
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(postConnection.getInputStream(),
+                    StandardCharsets.UTF_8))) {
+                StringBuilder content = new StringBuilder();
+                String input;
+                while ((input = reader.readLine()) != null) {
+                    content.append(input);
+                }
+                return content.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String makeURLWithParams(Map<String, String> params, String url) {
+        url += "?";
+        for (String key :
+                params.keySet()) {
+            url += key + "=" + params.get(key) + "&";
+        }
+        return url;
+    }
+}
---
+++ src/main/java/ru/kpfu/itis/gerasimov/net/Main.java
@@ -0,0 +1,80 @@
+package ru.kpfu.itis.gerasimov.net;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+////        try {
+////            URL url = new URL("http://jsonplaceholder.typicode.com/posts?userId=1");
+////            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//////            HashMap<String, String> parameters = new HashMap<>();
+//////            parameters.put("key", "value");
+//////            connection.setDoOutput(true);
+//////            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+//////                outputStream.writeBytes("userId=1");
+//////            }
+////            connection.setRequestMethod("GET");
+////            connection.setRequestProperty("Content-Type", "application/json");
+////            connection.setConnectTimeout(5000);
+////            connection.setReadTimeout(5000);
+////            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+////                StringBuilder content = new StringBuilder();
+////                String input;
+////                while ((input = reader.readLine()) != null) {
+////                    content.append(input);
+////                }
+////                System.out.println(content);
+////            }
+////            connection.disconnect();
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////        }
+//
+//        try {
+//            URL postURL = new URL("https://reqres.in/api/register");
+//            HttpURLConnection postConnection = (HttpURLConnection) postURL.openConnection();
+//
+//            postConnection.setRequestMethod("POST");
+//            postConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+//            postConnection.setRequestProperty("Accept", "application/json");
+//
+//            postConnection.setDoOutput(true);
+//            String jsonInputString = "\"email\": \"test\", \"password\": \"test\"";
+//            try(OutputStream outputStream = postConnection.getOutputStream()) {
+//                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+//                outputStream.write(input);
+//            }
+//
+//            try (BufferedReader reader = new BufferedReader(new InputStreamReader(postConnection.getInputStream(),
+//                    StandardCharsets.UTF_8))) {
+//                StringBuilder content = new StringBuilder();
+//                String input;
+//                while ((input = reader.readLine()) != null) {
+//                    content.append(input);
+//                }
+//                System.out.println(content);
+//            }
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        Map<String, String> header = new HashMap<>();
+        Map<String, String> params = new HashMap<>();
+        header.put("Accept", "application/json");
+        header.put("Content-Type", "application/json");
+        params.put("foo1", "bar1");
+        params.put("foo2", "bar2");
+        HttpClientImplementation httpClientImplementation = new HttpClientImplementation();
+
+//        System.out.println(httpClientImplementation.get("https://postman-echo.com/get", header, params));
+        System.out.println(httpClientImplementation.post("https://postman-echo.com/post", header, params));
+    }
+}
